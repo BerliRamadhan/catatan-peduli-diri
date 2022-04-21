7102d3254083e3103d7d2ba045f554ca43fbdae6
AzkaLufthansa <azkalufthansa3@gmail.com>
2022-04-21 12:00:33 +0700

    Menambahkan active link

--- resources/views/dashboard/partials/header.blade.php
+++ resources/views/dashboard/partials/header.blade.php
@@ -2,10 +2,11 @@
     <img src="/img/default.png" style="width: 150px" alt="Gambar Saya" class="img-thumbnail float-start me-4">
     <h1>Peduli Diri</h1>
     <p>Catatan Perjalanan</p>
+    
     <nav>
-        <a href="/home" class="text-decoration-none">Home </a> |
-        <a href="/catatan" class="text-decoration-none">Catatan Perjalanan </a> |
-        <a href="/buat" class="text-decoration-none">Isi Data </a> |
+        <a href="/home" class="text-decoration-none {{ Request::is('/home') ? 'text-decoration-underline' : '' }}">Home </a> |
+        <a href="/catatan" class="text-decoration-none {{ Request::is('/catatan') ? 'text-decoration-underline' : '' }}">Catatan Perjalanan </a> |
+        <a href="/buat" class="text-decoration-none {{ Request::is('/buat') ? 'text-decoration-underline' : '' }}">Isi Data </a> |
         <form action="/logout" method="POST" class="d-inline">
             @csrf
             <button type="submit" class="buttonliling" onclick="return confirm('Logout?')">Logout</button>
